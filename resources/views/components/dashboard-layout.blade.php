a49ee6a2dd4b79b6a5908e4d59f46c585e219c9d
Jan <limjaneriko@gmail.com>
2024-10-03 15:34:41 +0800

    Installed Carbon Blade icons and added theme colors in tailwind config file

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -9,6 +9,18 @@
 </head>
 
 <body>
-    {{ $slot }}
+    <header id='main-header' class='w-full flex flex-row justify-between items-center h-auto px-4 py-5 bg-brand-primary'>
+        <div id='header-lhs' class='flex flex-row items-center'>
+            <x-carbon-menu class='text-brand-text-dark-bg h-8'/>
+        </div>
+        <div id='header-rhs' class='flex flex-row justify-end items-center gap-x-3'>
+            <x-carbon-user class='text-brand-text-dark-bg h-8'/>
+            <p class='text-brand-text-dark-bg text-xl'>Welcome, </p>
+        </div>
+    </header>
+
+    <div>
+        {{ $slot }}
+    </div>
 </body>
 </html>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,3 +1,3 @@
-<x-layout>
+<x-dashboard-layout>
 
-</x-layout>
+</x-dashboard-layout>
