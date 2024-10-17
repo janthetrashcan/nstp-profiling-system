6e07b17701c52dd2472852896f3015f781ec0d49
Jan <limjaneriko@gmail.com>
2024-10-17 20:05:41 +0800

    Add student routing bugfix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController; 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
@@ -10,10 +10,9 @@ Route::redirect('/','/dashboard/students')->name('dashboard');
 // place dashboard routes here
 Route::middleware(['auth'])->group(function(){
     Route::get('/dashboard/students', [DashboardController::class, 'showStudentList'])->name('dashboard.studentlist');
-    Route::get('/dashboard/students/{s_id}', [DashboardController::class, 'showStudentProfile'])->name('dashboard.showstudent');
+    Route::get('/dashboard/students/add', [StudentController::class, 'addStudent'])->name('dashboard.addstudent');
+    Route::get('/dashboard/students/view/{s_id}', [DashboardController::class, 'showStudentProfile'])->name('dashboard.showstudent');
     Route::get('/dashboard/formators', [DashboardController::class, 'showFormatorList'])->name('dashboard.formatorlist');
-    Route::get('/dashboard/students/add', [StudentController::class, 'addStudent'])->name('dashboard.addStudent');
-    
 });
 
 Route::middleware('auth')->group(function () {
