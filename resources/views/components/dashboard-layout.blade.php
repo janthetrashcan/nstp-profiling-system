920fba9ad0322df1f42d5bfa9badcb7cf83dd9b5
Jan <limjaneriko@gmail.com>
2024-10-18 09:15:19 +0800

    Minor UI tweaks in studentadd view

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <script src="https://cdn.tailwindcss.com"></script>
+  @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-200">
 
--- resources/views/dashboard/studentadd.blade.php
+++ resources/views/dashboard/studentadd.blade.php
@@ -4,7 +4,7 @@
     @csrf
     <div class='flex flex-row pr-4 mb-3 justify-between'>
         <div id='functions-lhs' class='flex flex-row gap-x-3'>
-            <a href='{{ route('dashboard.studentlist') }}' class='bg-slate-200 flex flex-row w-fit h-12 px-4 py-2 justify-start items-center rounded-xl gap-2'>
+            <a href='{{ route('dashboard.studentlist') }}' class='bg-gray-100 flex flex-row w-fit h-12 px-4 py-2 justify-start items-center rounded-xl gap-2'>
                 <x-carbon-arrow-left class='h-6' />
                 <h1 class='font-semibold'>Return</h1>
             </a>
