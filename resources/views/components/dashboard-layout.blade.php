1065cbf8945694a2b051993229dd3d3911b6e8b0
Jan <janerikolim@gmail.com>
2024-12-19 10:52:53 +0800

    webserver

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
+  {{-- <link rel="stylesheet" href="/public/build/assets/app-CQMMHOql.css"> --}}
   @vite('resources/css/app.css')
-  @vite('resources/js/Pages/dashboard-script.js')
 </head>
 
 <body class="h-full">
